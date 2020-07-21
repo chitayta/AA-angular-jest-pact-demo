e340f6b93167e32180407017cbce20eed315760d
AAVN\tctay <tay.ta@axonactive.com>
2020-07-21 16:26:53 +0700

    # update tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,15 @@ describe('AppComponent', () => {
   it(`should have as title 'hello-world'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello-world');
+    expect(app.title).toMatchSnapshot(); // .toEqual('hello-world');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
+    
+    expect(compiled.querySelector('.content span')).toMatchSnapshot();
     expect(compiled.querySelector('.content span').textContent).toContain('hello-world app is running!');
   });
 });
